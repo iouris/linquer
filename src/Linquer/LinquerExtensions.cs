321761b2199d9d02cd626f2a4f2b608b93d48e15
iouri <iouris@gmail.com>
2021-12-24 13:15:08 +1300

    renamed LambdaExtensions to LinquerExtensions

--- src/Linquer/LinquerExtensions.cs
+++ src/Linquer/LinquerExtensions.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Linquer.Internals;
+using System;
 using System.Linq.Expressions;
-using Linquer.Internals;
 
 namespace Linquer
 {
-    public static class LambdaExtensions
+    public static class LinquerExtensions
     {
         public static Expression<Func<TArg1, bool>> Inline<TArg1>(this Expression<Func<TArg1, bool>> predicate, IExpressionMethodCallRewriter? expressionMethodCallRewriter = null)
         {
             var expressionVisitor = new LinquerExpressionVisitor(expressionMethodCallRewriter);
-            return expressionVisitor.VisitAndConvert(predicate, callerName: nameof(Inline));
+            var inlined = expressionVisitor.VisitAndConvert(predicate, callerName: nameof(Inline));
+            return inlined;
         }
     }
 }
