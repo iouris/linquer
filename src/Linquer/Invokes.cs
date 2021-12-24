fa5e347bb22e207655cb9215a5c0b30c4011d5ca
iouri <iouris@gmail.com>
2021-12-24 15:27:10 +1300

    Invokes unit tests

--- src/Linquer/Invokes.cs
+++ src/Linquer/Invokes.cs
@@ -7,13 +7,13 @@ namespace Linquer
     {
 #pragma warning disable IDE0060 // Remove unused parameter
         public static TRes Invoke<TArg1, TRes>(this Expression<Func<TArg1, TRes>> expression, TArg1 arg1) =>
-            throw new NotImplementedException();
+            throw new NotSupportedException($"Invokes.{nameof(Invoke)} should not be called, it is expected to be used in LINQ expressions only.");
 
         public static TRes Invoke<TArg1, TArg2, TRes>(this Expression<Func<TArg1, TArg2, TRes>> expression, TArg1 arg1, TArg2 arg2) =>
-            throw new NotImplementedException();
+            throw new NotSupportedException($"Invokes.{nameof(Invoke)} should not be called, it is expected to be used in LINQ expressions only.");
 
         public static TRes Invoke<TArg1, TArg2, TArg3, TRes>(this Expression<Func<TArg1, TArg2, TArg3, TRes>> expression, TArg1 arg1, TArg2 arg2, TArg3 arg3) =>
-            throw new NotImplementedException();
+            throw new NotSupportedException($"Invokes.{nameof(Invoke)} should not be called, it is expected to be used in LINQ expressions only.");
 #pragma warning restore IDE0060 // Remove unused parameter
     }
 }
---
+++ tests/Linquer/InvokesTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Linquer.Tests;
+
+public class InvokesTests
+{
+    [Fact]
+    public void InvokeWith1ArgumentShouldThrow()
+    {
+        var invoke = () => Invokes.Invoke((int _) => 0, default);
+        invoke.Should().ThrowExactly<NotSupportedException>().WithMessage("*should not be called*");
+    }
+
+    [Fact]
+    public void InvokeWith2ArgumentsShouldThrow()
+    {
+        var invoke = () => Invokes.Invoke((int _, int _) => 0, default, default);
+        invoke.Should().ThrowExactly<NotSupportedException>().WithMessage("*should not be called*");
+    }
+
+    [Fact]
+    public void InvokeWith3ArgumentsShouldThrow()
+    {
+        var invoke = () => Invokes.Invoke((int _, int _, int _) => 0, default, default, default);
+        invoke.Should().ThrowExactly<NotSupportedException>().WithMessage("*should not be called*");
+    }
+}
