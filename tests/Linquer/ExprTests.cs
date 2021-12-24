31313cbddb1104b8be88a8ea05703a7dc82af601
iouri <iouris@gmail.com>
2021-12-24 15:30:41 +1300

    Expr unit tests

---
+++ tests/Linquer/ExprTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Linquer.Tests;
+
+public class ExprTests
+{
+    [Fact]
+    public void CreateShouldReturnTheGivenExpression()
+    {
+        Expression<Func<int, int>> expr = a => a;
+        Expr.Create(expr).Should().BeSameAs(expr);
+    }
+}
