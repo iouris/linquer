112991b42cb38bf3d52d64214a47c17068faad37
iouri <iouris@gmail.com>
2021-12-26 12:47:55 +1300

    Register_with_no_method_call_in_the_body_should_throw unit test

---
+++ tests/Linquer/Registrations/InlineableMethodsRegisterExtensionsTests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+using Linquer.Registrations;
+using Xunit;
+
+namespace Linquer.Tests.Registrations;
+
+public class InlineableMethodsRegisterExtensionsTests
+{
+    [Fact]
+    public void Register_with_no_method_call_in_the_body_should_throw() =>
+        new Action(() => new InlineableMethodsRegister().Register(() => 1, _ => default!))
+        .Should().Throw<ArgumentException>().WithMessage("*expected to contain a method call*");
+}
