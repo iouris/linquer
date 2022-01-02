4e3abebdfbc1a340e2b1d8978e7f29f977d0cb02
iouri <iouris@gmail.com>
2022-01-02 18:14:09 +1300

    unit tests coverage fixed

--- tests/Linquer/ExprTests.cs
+++ tests/Linquer/ExprTests.cs
@@ -7,9 +7,30 @@ namespace Linquer.Tests;
 public class ExprTests
 {
     [Fact]
-    public void CreateShouldReturnTheGivenExpression()
+    public void Create0ShouldReturnTheGivenExpression()
+    {
+        Expression<Func<int>> expr = () => 0;
+        Expr.Create(expr).Should().BeSameAs(expr);
+    }
+
+    [Fact]
+    public void Create1ShouldReturnTheGivenExpression()
     {
         Expression<Func<int, int>> expr = a => a;
+        Expr.Create(expr).Should().BeSameAs(expr);
+    }
+
+    [Fact]
+    public void Create2ShouldReturnTheGivenExpression()
+    {
+        Expression<Func<int, int, int>> expr = (a, b) => a;
+        Expr.Create(expr).Should().BeSameAs(expr);
+    }
+
+    [Fact]
+    public void Create3ShouldReturnTheGivenExpression()
+    {
+        Expression<Func<int, int, int, int>> expr = (a, b, c) => a;
         Expr.Create(expr).Should().BeSameAs(expr);
     }
 }
