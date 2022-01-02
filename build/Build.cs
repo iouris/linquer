694bd8bd618053606cfde8f032940eb755b970a2
iouri <iouris@gmail.com>
2022-01-02 18:09:43 +1300

    nuke

---
+++ build/Build.cs
@@ -0,0 +1,128 @@
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Nuke.Common.Tools.Coverlet;
+using Nuke.Common.Tools.CoverallsNet;
+
+namespace Linquer.Build;
+
+[CheckBuildProjectConfigurations]
+[ShutdownDotNetAfterServerBuild]
+
+[GitHubActions(
+    "ci",
+    GitHubActionsImage.UbuntuLatest,
+    //auto-generate /ci/ci.yml
+    AutoGenerate = true,
+    OnPushBranches = new[] { "main" },
+    OnPullRequestBranches = new[] { "main" },
+    
+    InvokedTargets = new[] { nameof(RunUnitTests) },
+    //will generate CI env command to set corresponding env vars to values imported from secrets, ie
+    //TOKEN_1 = ${{ secrets.TOKEN_1 }}
+    ImportSecrets = new[] { "GITHUB_TOKEN" }
+)]
+public class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main() => Execute<Build>(build => build.RunUnitTests);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    private readonly Solution Solution = null!;
+
+    //[GitRepository]
+    //private readonly GitRepository GitRepository = null!;
+    //[GitVersion] readonly GitVersion GitVersion;
+
+    //RootDirectory is the repository folder ie ..\repos\linquer
+    private static readonly AbsolutePath SourceDirectory = RootDirectory / "src";
+    private static readonly AbsolutePath TestsDirectory = RootDirectory / "tests";
+    private static readonly AbsolutePath ArtifactsDirectory = RootDirectory / "artifacts";
+    private static readonly AbsolutePath LinquerTestsProjectDirectory = TestsDirectory / "Linquer";
+    private const string TestResultsDirectoryName = "TestResults";
+    private static readonly AbsolutePath LinquerTestResultsDirectory = LinquerTestsProjectDirectory / TestResultsDirectoryName;
+
+    public Target Clean => definition => 
+        definition
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    public Target Restore => definition => 
+        definition
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => 
+                s
+                .SetProjectFile(Solution)
+            );
+        });
+
+    public Target Compile => definition =>
+        definition
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s =>
+                s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                //.SetAssemblyVersion(GitVersion.AssemblySemVer)
+                //.SetFileVersion(GitVersion.AssemblySemFileVer)
+                //.SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore()
+            );
+        });
+
+    public Target RunUnitTests => definition =>
+        definition
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(s =>
+                s
+                .SetProjectFile(Solution)
+                .EnableNoBuild()
+                .SetVerbosity(DotNetVerbosity.Normal)
+
+                //unit tests code coverage
+                .EnableCollectCoverage()
+                .SetCoverletOutput(TestResultsDirectoryName + "/")
+                .SetCoverletOutputFormat(CoverletOutputFormat.lcov)
+            ); ;
+        });
+
+    public Target PublishCoverageReport => definition =>
+        definition
+        .DependsOn(RunUnitTests)
+        .Executes(() =>
+            CoverallsNetTasks.CoverallsNet(s =>
+                s
+                .SetInput(LinquerTestResultsDirectory / "coverage.info")
+                //note that the variable get set by the code line above:
+                //ImportSecrets = new[] { "GITHUB_TOKEN" }
+                .SetRepoTokenVariable("GITHUB_TOKEN")
+            )
+        );
+}
---
+++ build/Configuration.cs
@@ -0,0 +1,16 @@
+using Nuke.Common.Tooling;
+using System.ComponentModel;
+
+namespace Linquer.Build;
+
+[TypeConverter(typeof(TypeConverter<Configuration>))]
+public class Configuration : Enumeration
+{
+    public static readonly Configuration Debug = new() { Value = nameof(Debug) };
+    public static readonly Configuration Release = new() { Value = nameof(Release) };
+
+    public static implicit operator string(Configuration configuration)
+    {
+        return configuration.Value;
+    }
+}
