d6e9f2bc05b49a9755ee3acf297cc0d55984ceaa
iouri <iouris@gmail.com>
2022-02-12 17:04:12 +1300

    removed windows commands from build.cmd

--- build.cmd
+++ build.cmd
@@ -1,7 +1,3 @@
-:; set -eo pipefail
-:; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-:; ${SCRIPT_DIR}/build.sh "$@"
-:; exit $?
-
-@ECHO OFF
-powershell -ExecutionPolicy ByPass -NoProfile -File "%~dp0build.ps1" %*
+set -eo pipefail
+SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+${SCRIPT_DIR}/build.sh "$@"
